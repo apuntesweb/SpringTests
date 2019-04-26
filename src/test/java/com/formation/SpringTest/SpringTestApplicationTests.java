6d06ab119296396cf9f858633c996fd9c7789473
Roque rbugadev@gmail.com <rbugadev@gmail.com>
2019-04-26 16:36:31 +0200

    Contiene errores

--- src/test/java/com/formation/SpringTest/SpringTestApplicationTests.java
+++ src/test/java/com/formation/SpringTest/SpringTestApplicationTests.java
@@ -1,16 +1,25 @@
 package com.formation.SpringTest;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.formation.service.MessageComponent;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SpringTestApplicationTests {
+	
+	@Autowired
+	public MessageComponent messageProvider;
 
 	@Test
-	public void contextLoads() {
+	public void test1() {
+		assertEquals("Hello World!", messageProvider.getMessage());
 	}
 
 }
